ad20e17213cce9ad86e4d28a93bc6de1f2b20566
alialazzawi <ahbaker2014@gmail.com>
2021-04-07 00:27:38 +0300

    login ui complete

---
+++ BlogLab-UI/src/app/models/account/application-user.model.ts
@@ -0,0 +1,11 @@
+
+export class ApplicationUser {
+    constructor(
+        public ApplicationUserId: number,
+        public username: string,
+        public fullname :string,
+        public email: string,
+        public token : string
+    )
+    { }
+}
